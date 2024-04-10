d9936bce318052066330caea494ab8be2b711a28
Marcus Lindholm <hello@marcuslindholm.com>
2024-04-10 14:30:58 +0200

    fixat kodduplicering

--- client/client.js
+++ client/client.js
@@ -38,49 +38,8 @@ function ShowPurchasePage() {
       url: host + "/products", 
       type: "GET",
       success: function(response) {
-          response.forEach(function(product) {
-            var productHTML = `
-            <div class="col-md-4">
-                <div class="card">
-                  <img src="/product_images/${product.img}" class="card-img-top centered-product-image show-product" data-product-id="${product.id}" alt="Product Image">
-                    <div class="card-body">
-                        <h5 class="card-title show-product" data-product-id="${product.id}">${product.name}</h5>
-                        <p class="card-text">${product.description.length > 28 ? product.description.substring(0, 25) + '...' : product.description}</p>
-                        <p class="card-text"> ${product.price} kr</p>
-                        <div class="d-flex align-items-center mb-3">
-                            <label for="quantity" class="me-2"></label>
-                            <div class="input-group">
-                                <button class="btn btn-sm btn-outline-dark" onclick="this.parentNode.querySelector('input[type=number]').stepDown()" id="minus-button">
-                                    <i class="fas fa-minus"></i>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-                                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-                                    </svg>
-                                </button>
-                                <input type="number" id="quantity" class="form-control" value="1" min="1" max="${product.quantity}">
-                                <button class="btn btn-sm btn-outline-dark" onclick="this.parentNode.querySelector('input[type=number]').stepUp()" id="plus-button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                                    </svg>
-                                    <i class="fas fa-plus"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <button class="btn btn-light" style="width: 145px; margin: 5px 0;" ${product.quantity === 0 ? 'disabled' : ''}>Lägg i varukorg</button>
-                        <div class="product-overview">
-                          <button class="btn btn-dark" style="width: 105px;">Köp nu</button>
-                          <button class="btn btn-outline-dark">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-                              </svg>
-                          </button>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-            // Append product HTML to the container
-            $("#product-container").append(productHTML);
-        });
           populateFilterDropdowns(response);
+          refreshProducts();
       },
       error: function(error) {
           console.error("Error fetching products:", error);
@@ -98,8 +57,6 @@ function refreshProducts() {
 
       var filteredProducts = response.filter(function(product) {
         // Check if the product matches all selected filters
-        console.log(yearCheckboxesfilter);
-        console.log(product.year);
         return (
           (yearCheckboxesfilter.length === 0 || yearCheckboxesfilter.includes(product.year)) &&
           (sectionCheckboxesfilter.length === 0 || sectionCheckboxesfilter.includes(product.section)) &&
